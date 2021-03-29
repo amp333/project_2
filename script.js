96dab28caccf924bb10526416a53466be0d40237
amp333 <50529259+amp333@users.noreply.github.com>
2021-03-28 20:46:27 -0700

    Update script.js
    "made indentation"

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ $(document).ready(function () {
              
                      $ (".row").append(
                     `<div class='col-12 col-sm-6 col-md-4'>
-                    <button id='btn-delete' class='btn btn-danger w-100'>Delete</button>
-                    <div class='my-1 p-2'>
-                    <img class='card-img-top' alt='user' src='${data[i].picture.large}'>
-                    <h5 class='card-title'> ${data[i].name.first} ${data[i].name.last}</h5>
-                    <p class='card-text'> ${data[i].location.city} ${data[i].location.street.number} ${data[i].location.street.name}</p>
-                    <button class='btn btn-primary'>Email</button>
-                    </div>
-                    </div>
+                        <button id='btn-delete' class='btn btn-danger w-100'>Delete</button>
+                        <div class='my-1 p-2'>
+                            <img class='card-img-top' alt='user' src='${data[i].picture.large}'>
+                            <h5 class='card-title'> ${data[i].name.first} ${data[i].name.last}</h5>
+                            <p class='card-text'> ${data[i].location.city} ${data[i].location.street.number} ${data[i].location.street.name}</p>
+                            <button class='btn btn-primary'>Email</button>
+                        </div>
                     </div>`)
                 }
             }
